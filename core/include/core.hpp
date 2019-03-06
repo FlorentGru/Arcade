c7620b8f8854df29477eeb47ce84f51f51ed4df3
Dumaine <clementdumaine@epitech.eu>
2019-03-06 18:05:21 +0100

    Adding core

---
+++ core/include/core.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** OOP_arcade_2018
+** File description:
+** core.hpp
+*/
+
+#ifndef __CORE_HPP__
+#define __CORE_HPP__
+
+
+
+#endif
---
+++ core/srcs/core.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** OOP_arcade_2018
+** File description:
+** core.cpp
+*/
+
+#include "core.hpp"
+
+int main(int ac, char **av)
+{
+    return (0);
+}
