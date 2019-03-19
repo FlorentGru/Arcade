c2aac5d5e9eacef5baa4b1556f009c5ad38d8689
Dumaine <clementdumaine@epitech.eu>
2019-03-19 15:25:59 +0100

    Adding allegro

---
+++ lib/srcs/allegro/allegroDisplayModule.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2018
+** OOP_arcade_2018
+** File description:
+** allegroDisplayModule.cpp
+*/
+
+#include "allegroDisplayModule.hpp"
+
+bool arcDisplay::sfmlDisplayModule::display(const std::vector<std::reference_wrapper<IInfoDisplay>> &info)
+{
+
+}
+
+bool initScreen()
+{
+    
+}
