bf96e5955c5777cba28b16cf30d08d4a2aec966f
Florent gru <fgru@localhost.localdomain>
2019-03-21 11:44:38 +0100

    Add a little snake on ncurses

---
+++ games/srcs/Snake/snake.hpp
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2018
+** Test
+** File description:
+** snake.hpp
+*/
+
+#include <iostream>
+#include <vector>
+#include <unistd.h>
+#include <ncurses.h>
+#include <cstdlib>
+
+#ifndef _SNAKE_H_
+#define _SNAKE_H_
+
+struct snakebody {
+    int x;
+    int y;
+    snakebody(int col, int row);
+    snakebody();
+};
+
+class snakeclass {
+    public:
+    snakeclass();
+    ~snakeclass();
+    void start(); 
+
+    private:
+    int points;
+    int delay;
+    int maxwidth;
+    int maxheight;
+    char direction;
+    char snakepart;
+    char term;
+    char foo;
+    char wall;
+    bool food;
+    snakebody locFood;
+    std::vector <snakebody> snake;
+
+    void createFood();
+    bool collision();
+    void movesnake();
+};
+
+#endif //_SNAKE_H_
